1a68b8cabffe223014ef146985d19adf21dcb2a7
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2024-02-08 16:54:54 +0300

    unused import statements removed.

--- src/main/java/com/dreamgames/backendengineeringcasestudy/BackendEngineeringCaseStudyApplication.java
+++ src/main/java/com/dreamgames/backendengineeringcasestudy/BackendEngineeringCaseStudyApplication.java
@@ -1,6 +1,5 @@
 package com.dreamgames.backendengineeringcasestudy;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
