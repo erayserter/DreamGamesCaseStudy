3d93ab5b0dbe26a766b595244308e1ab5bff0050
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2024-02-11 14:51:03 +0300

    tournaments request data migrated into a dto.

--- src/main/java/com/dreamgames/backendengineeringcasestudy/controller/TournamentController.java
+++ src/main/java/com/dreamgames/backendengineeringcasestudy/controller/TournamentController.java
@@ -1,5 +1,6 @@
 package com.dreamgames.backendengineeringcasestudy.controller;
 
+import com.dreamgames.backendengineeringcasestudy.dto.UserRequest;
 import com.dreamgames.backendengineeringcasestudy.model.User;
 import com.dreamgames.backendengineeringcasestudy.service.TournamentService;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,14 @@ public class TournamentController {
     }
 
     @PostMapping("/enter")
-    public void enterTournament(@RequestBody UUID userId) {
+    public void enterTournament(@RequestBody UserRequest userRequest) {
+        UUID userId = userRequest.userId();
         tournamentService.enterTournament(userId);
     }
 
     @PostMapping("{id}/claim-reward")
-    public User claimReward(@PathVariable Long id, @RequestBody UUID userId) {
+    public User claimReward(@PathVariable Long id, @RequestBody UserRequest userRequest) {
+        UUID userId = userRequest.userId();
         return tournamentService.claimReward(id, userId);
     }
 }
